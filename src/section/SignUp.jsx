afe70a5645958c1cf968fb1c18fa849604b391e7
bilunabeel <bilunabeelmv@gmail.com>
2023-10-14 13:46:39 +0530

    added error management

--- src/section/SignUp.jsx
+++ src/section/SignUp.jsx
@@ -5,33 +5,30 @@ import auth from '../firebase/firebase';
 const SignUp = ({setShowSignUp, showSignUp}) => {
   const [email, setEmail] = useState ('');
   const [password, setPassword] = useState ('');
-  const [emailValid, setEmailValid] = useState (true);
-  const [passwordValid, setPasswordValid] = useState (true);
   const [error, setError] = useState (null);
 
   const handleSignUp = async e => {
     e.preventDefault ();
     try {
-      if (emailValid && passwordValid) {
-        await createUserWithEmailAndPassword (auth, email, password);
-        setError (null);
-        console.log ('Signed Up Successfully');
-      }
+
+          await createUserWithEmailAndPassword (auth, email, password);
+          setError (null);
+          console.log ();
+      
     } catch (error) {
-      setError (error.message);
-      console.log ('Error Signing Up: ', error);
+      setError (mapFirebaseErrorMessage(error.code));
+      console.log(error.code);
+      console.log(error.message);
     }
   };
 
-  const validateEmail = value => {
-    const isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test (value);
-    setEmailValid (isValid);
-  };
-
-  const validatePassword = value => {
-    const isValid = value.length >= 8;
-    setPasswordValid (isValid);
-  };
+  const mapFirebaseErrorMessage = (errorCode)=>{
+    const errorMessage = {
+        'auth/invalid-email': "Enter a Valid Email!",
+        'auth/weak-password': 'Password should be atleast 6 characters!',
+    }
+    return errorMessage[errorCode] || 'There is a problem!'
+  }
 
   return (
     <div className="w-80% bg-light-yellow rounded-full px-5">
@@ -42,22 +39,20 @@ const SignUp = ({setShowSignUp, showSignUp}) => {
           placeholder="Email"
           onChange={e => {
             setEmail (e.target.value);
-            validateEmail (e.target.value);
+          
           }}
         />
-        {!emailValid && <p>Please, Enter valid Email!</p>}
         <input
           type="password"
           value={password}
           placeholder="Password"
           onChange={e => {
             setPassword (e.target.value);
-            validatePassword (e.target.value);
           }}
-        />
-        {!passwordValid && <p>Password must be atleast 8 characters!</p>}
+          />
 
         <button type="submit">Sign Up</button>
+          {error && <p>{error}</p>}
       </form>
       <p className="cursor-pointer" onClick={() => setShowSignUp (!showSignUp)}>
         Already have an account?
